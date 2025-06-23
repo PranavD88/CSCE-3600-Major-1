7fa7d781a0c9143d4828121368239fb79423b0fe
mary4890 <maryadeeko1@yahoo.com>
2025-06-22 20:05:11 -0500

    Update rotate.c

---
+++ rotate.c
@@ -0,0 +1,11 @@
+//rotate.c
+//Mary Adeeko, mya0051, Group 3
+//CSCE 3600.001
+
+#include "major1.h"
+
+unsigned int rotate_right(unsigned int one, unsigned int two)
+{
+        two &=31;
+        return (one >> two) | (one << (32- two));
+}
