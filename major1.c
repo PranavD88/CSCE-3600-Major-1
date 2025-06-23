3a77c0c15050e16577b854d6926595a12f277679
mary4890 <maryadeeko1@yahoo.com>
2025-06-22 20:30:39 -0500

    Update major1.c

--- major1.c
+++ major1.c
@@ -42,7 +42,6 @@ int main() {
 
             case 3:
                 // Rotate
-                unsigned int rotate_right(unsigned int one, unsigned int two)
                 {
                     two &=31; //Limit the rotation between 0 and 31
                     return (one >> two) | (one << (32- two)); //Rotate to the right by 2
